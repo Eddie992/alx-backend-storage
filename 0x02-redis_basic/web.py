9f5c813a8cc6cd50702ba0981119b631b815f6a1
Eddie992 <ekataika2002@gmail.com>
2024-08-15 03:30:19 +0200

    counter reset issue

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,19 +13,18 @@ redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
+    '''Caches the output of fetched data and tracks the access count.
     '''
     @wraps(method)
     def invoker(url) -> str:
-        '''The wrapper function for caching the output.
+        '''The wrapper function for caching the output and tracking requests.
         '''
-        redis_store.incr(f'count:{url}')
+        redis_store.incr(f'count:{url}')  # Increment the counter for each access
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.setex(f'result:{url}', 10, result)  # Set the result with a 10-second expiration
         return result
     return invoker
 
